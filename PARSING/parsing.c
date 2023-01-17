892f2a0d23e8b58f7e08acc0be1022f74c26fe12
Moad TAOUSS <motaouss@e1r9p17.clusters.42paris.fr>
2023-01-17 19:25:18 +0100

    expand normalement fini (add_history -> parsing pour gerer l'histo des expands)

--- PARSING/parsing.c
+++ PARSING/parsing.c
@@ -6,7 +6,7 @@
 /*   By: motaouss <motaouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 17:31:52 by motaouss          #+#    #+#             */
-/*   Updated: 2023/01/16 19:20:48 by motaouss         ###   ########.fr       */
+/*   Updated: 2023/01/17 19:24:45 by motaouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int	parser(t_msh *msh)
 	if ((token_error(msh->line)))
 	{
 		printf("\x1b[31mSyntax Error\n");
+		add_history(msh->line);
 		return (0);
 	}
 	else if (msh->line[0] == '\0')
@@ -41,3 +42,7 @@ int	parser(t_msh *msh)
 		return (1);
 	}
 }
+
+// < ok = No such file or directory
+// multi exit ? fork ?
+// leak ft_redir_new (valgrind --leak-check=full)
